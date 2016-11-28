5c66dd80de2586b931f8142f066e1aef3cb92d36
Nicu Timofte <timofte.alexandru05@yahoo.com>
2016-11-28 12:55:11 +0300

    added mail-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {MailService} from "./mail.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  templateUrl: `<div>
+<app-simple-form></app-simple-form>
+{{mail.message}}
+</div> `
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'aLet`s get started!';
+
+  constructor(private mail:MailService){}
 }
